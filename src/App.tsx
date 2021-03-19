de2e6be521917956b610aa7d78e590b3bb42f61e
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-19 03:46:42 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>dtmoney</h1>;
+  return (
+    <>
+      <h1>dtmoney</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
